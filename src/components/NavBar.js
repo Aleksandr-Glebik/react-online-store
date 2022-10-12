134a9b3029b8f27b7bc4518b659a62e293e94760
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-12 14:46:08 +0300

    feat: add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,37 @@
+import React, { useContext } from "react"
+import { Context } from "../index"
+
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import {Button} from 'react-bootstrap'
+import { Link } from "react-router-dom"
+import { SHOP_ROUTE } from "../utils/consts"
+import { observer } from "mobx-react-lite"
+
+
+
+const NavBar = observer( () => {
+    const {user} = useContext(Context)
+    return (
+        <Navbar bg="dark" variant="dark">
+         <Container>
+            <Link style={{color: 'white', textDecoration: 'none' }} to={SHOP_ROUTE}>OnlineStore.by</Link>
+            {user.isAuth ?
+                  <Nav className="ml-auto" style={{color: 'white'}}>
+                    <Button variant={'outline-light'}>Админ панель</Button>
+                    <Button variant={'outline-light'} className="ml-4">Войти</Button>
+                  </Nav>
+                :
+                  <Nav className="ml-auto" style={{color: 'white'}}>
+                    <Button variant={'outline-light'}
+                            onClick={() => user.setIsAuth(true)}
+                    >Авторизация</Button>
+                  </Nav>
+            }
+         </Container>
+        </Navbar>
+    )
+})
+
+export default NavBar
