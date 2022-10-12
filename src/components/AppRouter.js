fc5c0b3f93a04e9fcef501b3a56d1b03cfd71d8e
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-12 12:55:22 +0300

    feat: add UserStore, add Context

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,13 +1,15 @@
-import React from "react"
+import React, { useContext } from "react"
 import {Route, Routes } from "react-router-dom"
+import { Context } from "../index"
 import { authRoutes, publicRoutes } from "../routes"
 
 const AppRouter = () => {
-    const isAuth = false
+    const {user} = useContext(Context)
 
+    console.log('user', user)
     return (
         <Routes>
-            {isAuth && authRoutes.map(({path, Element}) => {
+            {user.isAuth && authRoutes.map(({path, Element}) => {
                return <Route key={path} path={path} element={Element} />
             })}
             {publicRoutes.map(({path, Element}) => {
