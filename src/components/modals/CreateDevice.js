fec6f7951ba5c4c3d50bf968dcefdc1c098e34b6
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-14 13:00:42 +0300

    feat: add modal windows for comp CreateBrand and CreateType

---
+++ src/components/modals/CreateDevice.js
@@ -0,0 +1,37 @@
+import React from "react"
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+
+const CreateDevice = ({show, onHide}) => {
+    
+
+    return (
+        <Modal
+            show={show}
+            onHide={onHide}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Добавить новый вид устройства
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Control
+                    placeholder="Введите название типа устройства"
+                />
+            </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="outline-success" onClick={onHide}>Добавить</Button>
+          <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+}
+
+export default CreateDevice
